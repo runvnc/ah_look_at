d0faf52994255b44bf9fed4f8840b15407f34520
Jason Livesay <runvnc@gmail.com>
2025-03-31 05:44:50 -0500

    chatocr

--- src/ah_look_at/mod.py
+++ src/ah_look_at/mod.py
@@ -26,8 +26,13 @@ async def examine_image(full_image_path, context=None):
     return message
 
 
+ocr_prompt ="""Act as an advanced layout-aware OCR program:
+   - extract the text as markdown in semantic order from the image and 
+   - output ONLY the text from the image in your response
+   - do NOT put your output in a markdown code block, just raw markdown throughout""" 
+
 @service()
-async def chat_ocr(image_path, prompt="Act as an advanced layout-aware OCR program: extract the text as markdown in semantic order from the image and output ONLY the text from the image in your response", context=None):
+async def chat_ocr(image_path, prompt=ocr_prompt, context=None):
     try:
         img_content = await examine_image(image_path, context)
 
